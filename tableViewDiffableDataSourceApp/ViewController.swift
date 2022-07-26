8e379b5e113de871c8ba4a5e2014fe6dc4c294b4
anies1212 <all.for.me1212sna@gmail.com>
2022-07-26 23:02:27 +0900

    first commit

--- tableViewDiffableDataSourceApp/ViewController.swift
+++ tableViewDiffableDataSourceApp/ViewController.swift
@@ -7,12 +7,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    enum Section {
+        case first
+    }
+    struct Fruit: Hashable {
+        let title: String
+    }
+
+    var dataSource: UITableViewDiffableDataSource<Section,Fruit>!
+    var fruits = [Fruit]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Fruits"
+        tableView.delegate = self
+        bind()
+        view.addSubview(tableView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(alert))
     }
+
+    override func viewDidLayoutSubviews() {
+        tableView.frame = view.bounds
+    }
+
+    @objc func alert(){
+        let alert = UIAlertController(title: "Select", message: nil, preferredStyle: .actionSheet)
+        for x in 0...20 {
+            alert.addAction(UIAlertAction(title: "Fruits:\(x+1)", style: .default, handler: {[weak self] _ in
+                let fruits = Fruit(title: "Fruits:\(x+1)")
+                self?.fruits.append(fruits)
+                self?.updateDataSource()
+            }))
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+
+    private func bind(){
+        dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { tableView, indexPath, model -> UITableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            //title = Fruit Title
+            cell.textLabel?.text = model.title
+            return cell
+        })
+    }
+
+    private func updateDataSource(){
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Fruit>()
+        snapshot.appendSections([.first])
+        snapshot.appendItems(fruits)
+        dataSource.apply(snapshot, animatingDifferences: true, completion: nil)
+    }
+
+
 
 
 }
